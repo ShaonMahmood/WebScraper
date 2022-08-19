3fb662f88f30d7a7d4301a2702174ca0118e877c
MahmoodHabib <mahmood.habib.cuet@gmail.com>
2022-08-20 04:27:21 +0600

    update settings file for accepting ebay url and search item from env

--- web_crawler/settings.py
+++ web_crawler/settings.py
@@ -20,10 +20,10 @@ NEWSPIDER_MODULE = 'web_crawler.spiders'
 #USER_AGENT = 'web_crawler (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 3
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
@@ -92,9 +92,11 @@ ROBOTSTXT_OBEY = True
 
 # MONGO DB SETTINGS
 MONGO_HOST = os.environ.get("MONGO_HOST")
-MONGO_PORT = os.environ.get("MONGO_HOST")
+MONGO_PORT = int(os.environ.get("MONGO_PORT"))
 MONGO_DB_NAME = os.environ.get("MONGO_DB_NAME")
 MONGO_COLLECTION_NAME = os.environ.get("MONGO_COLLECTION_NAME")
+EBAY_SEARCH_ITEM = os.environ.get("EBAY_SEARCH_ITEM") 
+EBAY_URL = os.environ.get("EBAY_URL")
 
 ITEM_PIPELINES = {
   "web_crawler.pipelines.WebCrawlerPipeline": 500
